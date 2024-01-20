046c8ed6c00570c99a7ab50b20365cb2c87cf607
John Francis Bea <89569109+waranghawong@users.noreply.github.com>
2024-01-20 20:46:26 +0800

    /

--- index.php
+++ index.php
@@ -11,6 +11,9 @@ $saved_locations = new locationsCntrl();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>KML FILE UPLOAD</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
     UPLOAD KML HERE
@@ -22,10 +25,11 @@ $saved_locations = new locationsCntrl();
     </form>
     <br>
     <br>
-    <table class="" border = '1'>
+    <table class="table" border = '1'>
         <thead>
             <th>Name</th>
             <th>Description</th>
+            <th>Action</th>
         </thead>
         <tbody>
             
@@ -37,8 +41,9 @@ $saved_locations = new locationsCntrl();
             else{
                 foreach($saved_locations->getKMS() as $row){
                     echo '<tr>';
-                    echo ' <td><a href="'.$row['location'].'">'.$row['name'].'</a></td>';
+                    echo ' <td>'.$row['name'].'</td>';
                     echo ' <td>'.$row['description'].'</td>';
+                    echo '  <td><a href="sample.php?dir='.substr($row['location'], 3).'" target="_blank"><i class="fa fa-globe"></i>open</a></td>';
                     echo '</tr>';
                 }
             }
@@ -47,24 +52,24 @@ $saved_locations = new locationsCntrl();
             </tr>
         </tbody>
     </table>
-
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script>
-		load() 
-		function load() 
-			{
-			var map;
-			var geoXml;
-
-			if (GBrowserIsCompatible()) 
-			{
-				map = new GMap2(document.getElementById('map'));
-				map.addControl(new GSmallMapControl());
-				map.addControl(new GMapTypeControl());
-				geoXml = new GGeoXml('samplekml.kml');
-				map.addOverlay(geoXml);
-				map.setCenter(new GLatLng(47.613976,-122.345467), 13);
-			}
-			}
+		function getLocation(){
+      
+            console.log(event.target.id);
+            $.ajax({
+                type: "GET",
+                url: "sample.php" ,
+                data: {
+                    "location": id_value
+                },
+                success : function(e) {
+                        console.log(e);
+                }
+            });
+         
+        }
 	</script>
 </body>
 </html>
---
+++ sample.php
@@ -0,0 +1,9 @@
+<?php
+  // $demofile = fopen('kml_files/eado06ivn5index.kml', 'r') or die('File cannot be opened!');
+  // echo fread($demofile, filesize('kml_files/eado06ivn5index.kml'));
+  // fclose($demofile);
+  // $loc = $_POST['location'];
+  if(isset($_GET['dir'])){
+  exec('c:\WINDOWS\system32\cmd.exe /c START '.$_GET['dir'].'');
+}
+?>
