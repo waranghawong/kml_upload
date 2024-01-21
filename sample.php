d88ade8bb57cf4cfc3d330c157b704e5cb5d71da
John Francis Bea <89569109+waranghawong@users.noreply.github.com>
2024-01-21 12:41:16 +0800

    /

--- sample.php
+++ sample.php
@@ -3,7 +3,24 @@
   // echo fread($demofile, filesize('kml_files/eado06ivn5index.kml'));
   // fclose($demofile);
   // $loc = $_POST['location'];
-  if(isset($_GET['dir'])){
-    exec('c:\WINDOWS\system32\cmd.exe /c START '.$_GET['dir'].'');
+  // 
+
+  $user_agent = getenv("HTTP_USER_AGENT");
+  $os = '';
+  if(strpos($user_agent, "Win") !== FALSE){
+    if(isset($_GET['dir'])){
+        exec('c:\WINDOWS\system32\cmd.exe /c START '.$_GET['dir'].'');
+    }
   }
+  elseif(strpos($user_agent, "Mac") !== FALSE){
+    $os = "Mac";
+  }
+  else{
+    $os = 'linux';
+  }
+
+
+
+
+
 ?>
